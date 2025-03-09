d7971d141ae886faff5671c9b943833411ba5bf9
ABDULLAHAHMED1575 <abdullahmed1575@gmail.com>
2025-03-09 20:43:12 +0500

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,12 @@ app.use(
   })
 );
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
 app.options("*", cors());
 
 app.use("/api/users", userRouter);
