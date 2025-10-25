ef3f7d8c61d215de23169e3088d98369685c9c1d
ABDULLAHAHMED1575 <abdullahmed1575@gmail.com>
2025-10-25 21:03:22 +0500

    updated package.json

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ export const authGuard = async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
         try {
             const token = req.headers.authorization.split(" ")[1];
-
+            
             const { id } = verify(token, process.env.JWT_token);
 
             req.user = await User.findById(id).select("-password");
--- frontend/src/services/api/axios.js
+++ frontend/src/services/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://blog-81ec.onrender.com',
+    baseURL: 'http://localhost:8000/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
